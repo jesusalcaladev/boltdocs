2c64fba22411f5c65ab4c4da2d599ec884e857af
Jesus Alcala <jesusalcalarojas@gmail.com>
2026-03-06 18:04:52 -0400

    feat: establish initial monorepo structure by adding docs, cli, and core packages.

--- packages/cli/tsup.config.ts
+++ packages/cli/tsup.config.ts
@@ -1,22 +1,9 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: [
-    "src/node/index.ts",
-    "src/node/cli/index.ts",
-    "src/client/index.ts",
-    "src/client/ssr.tsx",
-  ],
+  entry: ["src/index.ts"],
   format: ["cjs", "esm"],
   dts: true,
   tsconfig: "./tsconfig.json",
   clean: true,
-  external: [
-    "vite",
-    "react",
-    "react-dom",
-    "react-router-dom",
-    "virtual:boltdocs-routes",
-    "virtual:boltdocs-config",
-  ],
 });
