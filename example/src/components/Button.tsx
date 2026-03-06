5360db1e3953fe479422543a8a03287cbde691c3
Jesus Alcala <jesusalcalarojas@gmail.com>
2026-03-06 00:13:20 -0400

    feat: remove example

--- example/src/components/Button.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./button.css";
-interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  children: React.ReactNode;
-}
-
-export const Button: React.FC<ButtonProps> = ({ children, ...props }) => {
-  return <button {...props}>{children}</button>;
-};
