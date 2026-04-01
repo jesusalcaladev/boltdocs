4a4780e40987087b286a22b8065bdd429c7fdd2c
Jesus Alcala <jesusalcalarojas@gmail.com>
2026-03-31 22:38:18 -0400

    chore: updated files & updated entry

--- packages/core/tsup.config.ts
+++ packages/core/tsup.config.ts
@@ -1,23 +1,31 @@
-import { defineConfig } from "tsup";
+import { defineConfig } from 'tsup'
 
 export default defineConfig({
   entry: [
-    "src/node/index.ts",
-    "src/node/cli/index.ts",
-    "src/client/index.ts",
-    "src/client/ssr.tsx",
+    'src/node/index.ts',
+    'src/client/index.ts',
+    'src/client/ssr.tsx',
+    'src/client/hooks/index.ts',
+    'src/client/components/mdx/index.ts',
   ],
-  format: ["cjs", "esm"],
+  format: ['cjs', 'esm'],
   dts: true,
-  tsconfig: "./tsconfig.json",
+  tsconfig: './tsconfig.json',
   clean: true,
+  minify: true,
+  minifySyntax: true,
+  minifyWhitespace: true,
+  minifyIdentifiers: true,
   shims: true,
   external: [
-    "vite",
-    "react",
-    "react-dom",
-    "react-router-dom",
-    "virtual:boltdocs-routes",
-    "virtual:boltdocs-config",
+    'vite',
+    'react',
+    'react-dom',
+    'react-router-dom',
+    'virtual:boltdocs-routes',
+    'virtual:boltdocs-config',
+    'virtual:boltdocs-layout',
+    'virtual:boltdocs-mdx-components',
+    'virtual:boltdocs-entry',
   ],
-});
+})
