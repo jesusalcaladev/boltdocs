c14927055bd7fc0c49463a5fa0c46214d4cc8d0d
Jesus Alcala <jesusalcalarojas@gmail.com>
2026-04-10 11:12:25 -0400

    feat: add Hero component and define roadmap feature list structure

--- docs/src/roadmap/list-features.ts
+++ docs/src/roadmap/list-features.ts
@@ -1,66 +1,72 @@
-type FeatureStatus = "planned" | "in-progress" | "completed" | "released";
-type FeatureCategory = "Core" | "Integrations" | "CLI" | "UI" | "API" | "Ecosystem";
+type FeatureStatus = 'planned' | 'in-progress' | 'completed' | 'released'
+type FeatureCategory =
+  | 'Core'
+  | 'Integrations'
+  | 'CLI'
+  | 'UI'
+  | 'API'
+  | 'Ecosystem'
 
 export interface Feature {
-    name: string;
-    description: string;
-    status: FeatureStatus;
-    category: FeatureCategory;
+  name: string
+  description: string
+  status: FeatureStatus
+  category: FeatureCategory
 }
 
 export const FEATURES: Feature[] = [
-    {
-        name: "CodeSandbox Integration",
-        description: "Allow users to run code examples in CodeSandbox",
-        status: "planned",
-        category: "Integrations"
-    },
-    {
-        name: "Support Links",
-        description: "Add support links to the documentation",
-        status: "planned",
-        category: "Core"
-    },
-    {
-        name: "Gtag Integration",
-        description: "Support for google analytics",
-        status: "planned",
-        category: "Integrations"
-    },
-    {
-        name: "Algolia Integration",
-        description: "Support for algolia search",
-        status: "planned",
-        category: "Integrations"
-    },
-    {
-        name: "Component Banner",
-        description: "Add banner to components",
-        status: "planned",
-        category: "UI"
-    },
-    {
-        name: "Component Collapse",
-        description: "Allow users to collapse components",
-        status: "planned",
-        category: "UI"
-    },
-    {
-        name: "Export to PDF, EPUB, MOBI",
-        description: "Allow users to export documentation to PDF, EPUB, MOBI",
-        status: "planned",
-        category: "Core"
-    },
-    {
-        name: "Feedback System",
-        description: "Allow users to give feedback on documentation",
-        status: "planned",
-        category: "Core"
-    },
-    {
-        name: "OpenAPI & Swagger Integration",
-        description: "Allow users to import OpenAPI & Swagger specifications",
-        status: "planned",
-        category: "Integrations"
-    },
+  {
+    name: 'CodeSandbox Integration',
+    description: 'Allow users to run code examples in CodeSandbox',
+    status: 'in-progress',
+    category: 'Integrations',
+  },
+  {
+    name: 'Support Links',
+    description: 'Add support links to the documentation',
+    status: 'in-progress',
+    category: 'Core',
+  },
+  {
+    name: 'Gtag Integration',
+    description: 'Support for google analytics',
+    status: 'in-progress',
+    category: 'Integrations',
+  },
+  {
+    name: 'Algolia Integration',
+    description: 'Support for algolia search',
+    status: 'planned',
+    category: 'Integrations',
+  },
+  {
+    name: 'Component Banner',
+    description: 'Add banner to components',
+    status: 'planned',
+    category: 'UI',
+  },
+  {
+    name: 'Component Collapse',
+    description: 'Allow users to collapse components',
+    status: 'planned',
+    category: 'UI',
+  },
+  {
+    name: 'Export to PDF, EPUB, MOBI',
+    description: 'Allow users to export documentation to PDF, EPUB, MOBI',
+    status: 'planned',
+    category: 'Core',
+  },
+  {
+    name: 'Feedback System',
+    description: 'Allow users to give feedback on documentation',
+    status: 'planned',
+    category: 'Core',
+  },
+  {
+    name: 'OpenAPI & Swagger Integration',
+    description: 'Allow users to import OpenAPI & Swagger specifications',
+    status: 'planned',
+    category: 'Integrations',
+  },
 ]
