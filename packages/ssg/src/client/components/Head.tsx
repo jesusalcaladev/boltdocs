b156a21c75276ba88d7524a9e396d6b05961a950
Jesus Alcala <jesusalcalarojas@gmail.com>
2026-04-24 01:45:17 -0400

    refactor: update Head component to dynamically import Helmet from react-helmet-async

--- packages/ssg/src/client/components/Head.tsx
+++ packages/ssg/src/client/components/Head.tsx
@@ -1,6 +1,14 @@
 import type { ReactNode } from 'react'
+import type { ComponentType } from 'react'
 import type { HelmetProps } from 'react-helmet-async'
-import { Helmet } from 'react-helmet-async'
+import * as ReactHelmetAsync from 'react-helmet-async'
+
+type HelmetModule = {
+  Helmet?: ComponentType<HelmetProps>
+  default?: { Helmet?: ComponentType<HelmetProps> }
+}
+const helmetModule = ReactHelmetAsync as unknown as HelmetModule
+const Helmet = helmetModule.Helmet || helmetModule.default?.Helmet || (() => null)
 
 export type Props = HelmetProps & { children: ReactNode }
 
